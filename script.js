300cc0d2af25b7bcf9f456ea0cc9b3165f715db7
Raymond92-hub <sadeklo@st.vvu.edu.gh>
2022-08-31 09:40:34 -0700

    updated script.js

--- script.js
+++ script.js
@@ -21,3 +21,11 @@ for (let i = 0; i < btnshowMondal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e)
+{
+    if (e.key === "Escape")
+    {
+        closeModal(); 
+        }
+})
